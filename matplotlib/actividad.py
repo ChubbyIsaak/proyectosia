a39bcbcaff0c607da34a8111a8449d4cf54cf1bb
ChubbyIsaak <chubbyisaac908@gmail.com>
2023-07-17 09:15:12 -0600

    Examen

--- matplotlib/actividad.py
+++ matplotlib/actividad.py
@@ -1,23 +1,18 @@
 import matplotlib.pyplot as plt
 
-# Etiquetas de las categorías
 categorias = [
-    "Geografia  Aprobados",
-    "Geografia Reprobados",
-    "Matematicas Aprobados",
-    "Matematicas Reprobados",
-    "Lenguas Aprobados",
-    "Lenguas Reprobados",
+    "Geografia Aprobados"
+    "Geografia Reprobados"
+    "Matematicas Aprobados"
+    "Matematicas Reprobados"
+    "Lenguas Aprobados"
+    "Lenguas Reprobados"
 ]
 
-# Valores correspondientes a cada categoría
 valores = [25, 5, 15, 17, 29, 1]
 
-# Crear el gráfico de torta
 plt.pie(valores, labels=categorias)
 
-# Agregar título
-plt.title("Alumnos Aprobados/Reprobados")
+plt.title("Alumnos Aprobados y Reprobados de la Maestra Ana")
 
-# Mostrar el gráfico
 plt.show()
