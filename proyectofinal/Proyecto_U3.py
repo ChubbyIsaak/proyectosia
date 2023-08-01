8e19da8dbd7d09344fa400159736879b267e9381
ChubbyIsaak <chubbyisaac908@gmail.com>
2023-07-31 20:49:13 -0600

    .

--- proyectofinal/Proyecto_U3.py
+++ proyectofinal/Proyecto_U3.py
@@ -10,61 +10,61 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 
-# Load the dataset specifying the delimiter
+# Carga el set de datos especificando el delimitador
 df = pd.read_csv("Laboratorio_dataset_car.csv", delimiter=";")
 
-# Plotting bar charts for each categorical feature
+# Creando las barras de cada columnna
 for column in df.columns:
     plt.figure(figsize=(10, 4))
     sns.countplot(data=df, x=column)
     plt.title(f"Distribución de {column}")
     plt.show()
 
-# Encode categorical features
+# Se hace Encode a las caracteristicas de cada categoria
 le = LabelEncoder()
 df_encoded = df.apply(le.fit_transform)
 
-# Split the dataset into training and test sets
+# Divide los datos entre el entrenamiento y los sets de prueba
 X = df_encoded.drop("class", axis=1)
 y = df_encoded["class"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Initialize models
+# Se inicializan los modelos
 tree_model = DecisionTreeClassifier(random_state=42)
 forest_model = RandomForestClassifier(random_state=42)
 
-# Train models
+# Se entrenan los modelos
 tree_model.fit(X_train, y_train)
 forest_model.fit(X_train, y_train)
 
-# Make predictions
+# Se realizan las predicciones
 tree_pred = tree_model.predict(X_test)
 forest_pred = forest_model.predict(X_test)
 
-# Evaluate models
+# Se evaluan los modelos
 tree_accuracy = accuracy_score(y_test, tree_pred)
 forest_accuracy = accuracy_score(y_test, forest_pred)
 
-# Print accuracy
+# Se imprime la precision
 print("Precision del Arbol de Decisiones: ", tree_accuracy)
 print("Precision del Bosque Aleatorio: ", forest_accuracy)
 
-# Compute confusion matrix and classification report for each model
+# Se computa la matriz de confusion y el reporte de clasificacion de cada modelo
 tree_conf_matrix = confusion_matrix(y_test, tree_pred)
 forest_conf_matrix = confusion_matrix(y_test, forest_pred)
 
 tree_class_report = classification_report(y_test, tree_pred)
 forest_class_report = classification_report(y_test, forest_pred)
 
-# Print confusion matrices
+# Se imprimen las matricez de confusion
 print("Matriz de Confusion para el Arbol de Decision:\n")
 print(tree_conf_matrix)
 print("\nMatriz de Confusion para el Bosque Aleatorio:\n")
 print(forest_conf_matrix)
 
-# Print classification reports
+# Se imprimen los reportes de clasificacion
 print("\nReporte de Clasificacion para el Arbol de Decision:\n")
 print(tree_class_report)
 print("\nReporte de Clasificacion para el Bosque Aleatorio:\n")
